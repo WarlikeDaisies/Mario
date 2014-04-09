6a475f706cfe599d3422abe0aebc0f136b25bc31
VonBroccolighast <liambrockley@Liam-Brockleys-MacBook-Pro.local>
2014-04-09 12:44:55 -0700

    1

---
+++ src/mario/Scenes.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mario;
+
+/**
+ *
+ * @author liambrockley
+ */
+public enum Scenes {
+    
+    Home, Scene1, Scene2, Scene3, Scene4, Scene5, Scene6,Boss
+    
+}
