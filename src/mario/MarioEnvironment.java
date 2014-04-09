877732e50f99ee62643ac9992d2a59762bf5bed8
Victor <shalaurimm@hotmail.com>
2014-04-08 20:39:07 -0700

    Title screen outline

--- src/mario/MarioEnvironment.java
+++ src/mario/MarioEnvironment.java
@@ -5,6 +5,8 @@
 package mario;
 
 import environment.Environment;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
@@ -42,6 +44,16 @@ class MarioEnvironment extends Environment {
 
     @Override
     public void paintEnvironment(Graphics graphics) {
-
+        graphics.setColor(Color.red);
+        graphics.drawRect(800, 700, 350, 65);
+        graphics.drawRect(800, 400, 350, 65);
+        
+        graphics.setFont(new Font("Calabri", Font.BOLD, 45));
+        graphics.drawString("New Game", 860, 450);
+        graphics.drawString("Instructions", 850, 750);
+        
+        graphics.setColor(Color.BLUE);
+        graphics.setFont(new Font("Comic Sans", Font.ITALIC, 65));
+        graphics.drawString("Mapleland", 818, 200);
     }
 }
